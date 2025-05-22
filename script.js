8a76ef801061321e9117bcaa9d8a1c2fa44d6fad
sabapailodze04 <sabapailodze2004@gmail.com>
2025-05-22 05:55:08 -0700

    js

---
+++ script.js
@@ -0,0 +1,44 @@
+// // let username = "giiorgi";
+// // console.log(typeof username);
+// // let age = 30;
+// // console.log(typeof age);
+// // let isstudent = true;
+// // console.log(typeof isstudent);
+// // let testone = null;
+// // console.log(typeof testone);
+// // let job;
+// // console.log(typeof job);
+// let obj = {
+//   id: 1234,
+//   username: "mindia",
+//   isstudent: false,
+//   email: "sabapailodze2004@gmail.com",
+//   recoveremail: null,
+//   adress: {
+//     country: "georgia",
+//     city: "tbilisi",
+//     street: "rustaveli",
+//   },
+// };
+// console.log(obj.username);
+// console.log(obj.id);
+// console.log(obj.email);
+// console.log(obj.recoveremail);
+// console.log(obj.isstudent);
+// console.log(obj.adress.country);
+
+// // console.log(obj["adress"]["street"]);
+
+// let userarray = ["mindia "]
+
+let temperature = 38;
+
+if (temperature > 30) {
+  console.log("ცხელა მზიანი ამინდია");
+} else if (temperature < 0) {
+  console.log("ცივა ცუდი ამინდია");
+} else if (temperature > 0 && temperature < 30) {
+  console.log("ნორმალური ამინდია");
+} else {
+  console.log("დაფიქსირდა შეცდომა სცადეთ თავიდან");
+}
